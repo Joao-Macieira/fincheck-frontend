cf320d94a3bb86678e057316b48e20c9da07b5b9
João Macieira <jp.macieira@uol.com.br>
2023-07-26 11:14:09 -0300

    feat: auth layouta

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -3,14 +3,17 @@ import { AuthGuard } from './AuthGuard';
 import { Login } from '../view/pages/Login';
 import { Register } from '../view/pages/Register';
 import { Dashboard } from '../view/pages/Dashboard';
+import { AuthLayout } from '../view/layouts/AuthLayout';
 
 export function Router() {
   return (
     <BrowserRouter>
       <Routes>
         <Route element={<AuthGuard isPrivate={false} />}>
-          <Route path='/login' element={<Login />} />
-          <Route path='/register' element={<Register />} />
+          <Route element={<AuthLayout />}>
+            <Route path='/login' element={<Login />} />
+            <Route path='/register' element={<Register />} />
+          </Route>
         </Route>
 
         <Route element={<AuthGuard isPrivate />}>
