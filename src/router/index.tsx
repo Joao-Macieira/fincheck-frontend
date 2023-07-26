6bb180eada4c6542c9b4b4200fd6495c5f508012
João Macieira <jp.macieira@uol.com.br>
2023-07-26 11:07:28 -0300

    feat: configure auth guard for routes

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,3 +1,22 @@
+import { Routes, Route, BrowserRouter  } from 'react-router-dom';
+import { AuthGuard } from './AuthGuard';
+import { Login } from '../view/pages/Login';
+import { Register } from '../view/pages/Register';
+import { Dashboard } from '../view/pages/Dashboard';
+
 export function Router() {
-  return <h1>Router</h1>
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route element={<AuthGuard isPrivate={false} />}>
+          <Route path='/login' element={<Login />} />
+          <Route path='/register' element={<Register />} />
+        </Route>
+
+        <Route element={<AuthGuard isPrivate />}>
+          <Route path='/' element={<Dashboard />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
