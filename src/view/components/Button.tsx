2e0ca00e427d9a8078e667829dc8ae5e570ee042
João Macieira <jp.macieira@uol.com.br>
2023-07-27 16:17:42 -0300

    feat: ass spinner in button

--- src/view/components/Button.tsx
+++ src/view/components/Button.tsx
@@ -1,19 +1,23 @@
 import { ComponentProps } from "react";
 import { cn } from "../../app/utils/cn";
+import { Spinner } from "./Spinner";
 
 interface ButtonProps extends ComponentProps<'button'> {
   isLoading?: boolean;
 }
 
-export function Button({ className, isLoading, disabled, ...props }: ButtonProps) {
+export function Button({ className, isLoading, disabled, children, ...props }: ButtonProps) {
   return (
     <button
       {...props}
       disabled={isLoading ?? disabled}
       className={cn(
-        "bg-teal-900 px-6 h-12 rounded-2xl text-white font-medium hover:bg-teal-800 disabled:bg-gray-100 disabled:text-gray-400 disabled:cursor-not-allowed transition-all",
+        "bg-teal-900 px-6 h-12 rounded-2xl text-white font-medium hover:bg-teal-800 disabled:bg-gray-100 disabled:text-gray-400 disabled:cursor-not-allowed transition-all flex items-center justify-center",
         className
       )}
-    />
+    >
+      {!isLoading && children}
+      {isLoading && <Spinner className="w-6 h-6" />}
+    </button>
   )
 }
