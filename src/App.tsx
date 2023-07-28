1a7d6c63f2275bd9c49cfbb13ffa777931108546
João Macieira <jp.macieira@uol.com.br>
2023-07-28 11:56:47 -0300

    feat: add react query devtools

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,28 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { Router } from "./router";
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { Toaster } from "react-hot-toast";
 
-const queryClient = new QueryClient();
+import { Router } from "./router";
+import { AuthProvider } from "./app/contexts/AuthContext";
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      retry: false,
+      refetchOnWindowFocus: false,
+    }
+  }
+});
 
 export function App() {
   return (
     <QueryClientProvider client={queryClient}>
-      <Router />
-      <Toaster />
+      <AuthProvider>
+        <Router />
+        <Toaster />
+      </AuthProvider>
+
+      <ReactQueryDevtools />
     </QueryClientProvider>
   )
 }
