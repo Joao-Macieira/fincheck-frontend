0fe62cf5195cf8c344c18bfee1eecdc7c74c0f61
João Macieira <jp.macieira@uol.com.br>
2023-08-03 11:37:35 -0300

    feat: user menu dropdown

--- src/view/components/UserMenu.tsx
+++ src/view/components/UserMenu.tsx
@@ -1,9 +1,23 @@
+import { ExitIcon } from '@radix-ui/react-icons';
+import { DropdownMenu } from './DropdownMenu';
+
 export function UserMenu() {
   return (
-    <div className="bg-teal-0 rounded-full w-12 h-12 flex items-center justify-center border border-teal-100">
-      <span className="text-sm tracking-[-0.5px] text-teal-900 font-medium">
-        JP
-      </span>
-    </div>
+    <DropdownMenu.Root>
+      <DropdownMenu.Trigger>
+        <div className="bg-teal-0 rounded-full w-12 h-12 flex items-center justify-center border border-teal-100">
+          <span className="text-sm tracking-[-0.5px] text-teal-900 font-medium">
+            JP
+          </span>
+        </div>
+      </DropdownMenu.Trigger>
+
+      <DropdownMenu.Content classname='w-32'>
+        <DropdownMenu.Item classname='flex items-center justify-between'>
+          Sair
+          <ExitIcon className='w-4 h-4'/>
+        </DropdownMenu.Item>
+      </DropdownMenu.Content>
+    </DropdownMenu.Root>
   )
 }
