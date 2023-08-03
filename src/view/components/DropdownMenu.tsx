6f3106ac49f7618e4b82fb6b79e4680c904acdbe
João Macieira <jp.macieira@uol.com.br>
2023-08-03 11:37:16 -0300

    feat: dropdown menu

---
+++ src/view/components/DropdownMenu.tsx
@@ -0,0 +1,67 @@
+import * as RadixDropdownMenu from '@radix-ui/react-dropdown-menu';
+import { cn } from '../../app/utils/cn';
+
+function DropdownMenuRoot({ children }: { children: React.ReactNode }) {
+  return (
+    <RadixDropdownMenu.Root>
+      {children}
+    </RadixDropdownMenu.Root>
+  )
+}
+
+function DropdownMenuTrigger({ children }: { children: React.ReactNode }) {
+  return (
+    <RadixDropdownMenu.Trigger
+      className='outline-none'
+    >
+      {children}
+    </RadixDropdownMenu.Trigger>
+  )
+}
+
+interface DropdownMenuContentProps {
+  children: React.ReactNode;
+  classname?: string
+}
+
+function DropdownMenuContent({ children, classname }: DropdownMenuContentProps) {
+  return (
+    <RadixDropdownMenu.Portal>
+      <RadixDropdownMenu.Content
+        className={
+          cn(
+            'p-2 rounded-2xl bg-white space-y-2 shadow-[0px_11px_20px_0px_rgba(0,0,0,0.10)]',
+            classname
+          )
+        }
+      >
+        {children}
+      </RadixDropdownMenu.Content>
+    </RadixDropdownMenu.Portal>
+  )
+}
+
+interface DropdownMenuItemProps {
+  children: React.ReactNode;
+  classname?: string
+}
+
+function DropdownMenuItem({ children, classname }: DropdownMenuItemProps) {
+  return (
+    <RadixDropdownMenu.Item
+      className={cn(
+        'min-h-[48px] outline-none p-4 text-sm text-gray-800 rounded-2xl hover:bg-gray-50 transition-colors',
+        classname,
+      )}
+    >
+      {children}
+    </RadixDropdownMenu.Item>
+  )
+}
+
+export const DropdownMenu = {
+  Root: DropdownMenuRoot,
+  Trigger: DropdownMenuTrigger,
+  Content: DropdownMenuContent,
+  Item: DropdownMenuItem,
+};
